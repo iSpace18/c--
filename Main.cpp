4327bbd148ca6dc0f27299cf5e4b8404fea6d216
iSpace18 <steamdavid2007@gmail.com>
2025-01-24 10:08:33 +0300

    123

--- Main.cpp
+++ Main.cpp
@@ -9,15 +9,17 @@ int main(){
     // auto const & male{"Мужской"};
     // std::cout <<name,data,male;
     // std::cout<<hw;
-    int First_num = 0;
+  int First_num = 0;
     std::cout << "Введите первое число: ";
     std::cin >> First_num;
-    std::cout << "Ваше число: " << First_num;
+
     int Second_num = 0;
+    std::cout << "Введите второе число: ";
     std::cin >> Second_num;
-    std::cout << "Ваше число: " << Second_num;
-    std::cout << First_num - Second_num;
-    std::cout << First_num + Second_num;
-    std::cout << First_num * Second_num;
+
+    std::cout << "Ваши числа: " << First_num << " и " << Second_num << std::endl;
+    std::cout << "Разность: " << First_num - Second_num << std::endl;
+    std::cout << "Сумма: " << First_num + Second_num << std::endl;
+    std::cout << "Произведение: " << First_num * Second_num << std::endl;
     return 0;
 }
